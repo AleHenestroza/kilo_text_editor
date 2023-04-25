d5a4b5e63a3b084bb86351d2db76860bb3a52135
alehenestroza <alehenestroza@gmail.com>
2023-04-24 22:43:17 -0300

    Move cursor to top-left position

--- kilo.c
+++ kilo.c
@@ -16,6 +16,7 @@
 /*** data ***/
 
 struct editorConfig {
+    int cx, cy;
     int screenrows;
     int screencols;
     struct termios orig_termios;
@@ -164,8 +165,11 @@ void editorRefreshScreen() {
     abAppend(&ab, "\x1b[H", 3);
 
     editorDrawRows(&ab);
+    
+    char buf[32];
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1); // Move cursor position to 0;0
 
-    abAppend(&ab, "\x1b[H", 3);
+    abAppend(&ab, buf, strlen(buf));
     abAppend(&ab, "\x1b[?25h", 6); // Show cursor
 
     write(STDOUT_FILENO, ab.b, ab.len);
@@ -189,6 +193,8 @@ void editorProcessKeypress() {
 /*** init ***/
 
 void initEditor() {
+    E.cx = 0;
+    E.cy = 0;
     if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 
